ffae2ca3aab26f560ab037a6d50917536b0b484c
Uday-Pullagala6 <128678483+Uday-Pullagala6@users.noreply.github.com>
2024-10-04 22:02:32 +0530

    Update word count.java

--- word count.java
+++ word count.java
@@ -5,16 +5,15 @@ public class WordFrequencyCounter {
 
     public static void main(String[] args) {
 
-        // Define the sentence
+    
         String sentence = "The quick brown fox jumps over the lazy dog";
 
-        // Convert the sentence to lowercase and split it into words
         String[] words = sentence.toLowerCase().split("\\W+");
 
-        // Create a map to store the frequency of each word
+       
         Map<String, Integer> frequencyMap = new HashMap<>();
 
-        // Count the frequency of each word
+        
         for (String word : words) {
             if (frequencyMap.containsKey(word)) {
                 frequencyMap.put(word, frequencyMap.get(word) + 1);
@@ -23,7 +22,7 @@ public class WordFrequencyCounter {
             }
         }
 
-        // Print the frequency of each word
+       
         System.out.println("Word Frequency:");
         for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
